5716c0371c20832e55156e1d30d03f857128a5b7
Jonas Inocencio <s3787701@student.rmit.edu.au>
2025-07-09 21:39:07 +0100

    json file structure added

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,3 +1,33 @@
+import json
+
 class FileManager:
     def __init__(self):
         self.exercises = []
+        # if the file exists, boolean that it exists
+
+    def read_file(self):
+        print("TEST")
+
+        # pref.json
+        #   preferences = {
+        #       "type": "value"
+        #   }
+
+        # exercises.json
+        #   exercises = {
+        #       "exercise": "name",
+        #       "sets": "number",
+        #       "reps": "number",
+        #       "weight": "number"
+        #       "prev_weight": "number"
+        #   }
+
+        # ADVANCED
+        #   exercises = {
+        #       "exercise": "name",
+        #       "sets": integer,
+        #       "set_data": tuple(weight, prev_weight)
+
+    def save_file(self):
+        print("TEST")
+        #PLACEHOLDER
