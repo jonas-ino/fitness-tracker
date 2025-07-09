69d20a5bb7fd8b4831693f8106a976d523c86c74
Jonas Inocencio <s3787701@student.rmit.edu.au>
2025-07-09 15:14:04 +0100

    basic implementation of the exercise class

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from src.timer import Timer
+#from src.reader import Reader
+from src.exercise import Exercise
 
 if __name__ == "__main__":
     test_timer = Timer()
-    test_timer.run()
+
+    test_exercise = Exercise("TEST")
+    test_exercise.set_sets(3)
+    test_exercise.set_reps(10)
+    test_exercise.set_weight(40)
+
+    print(f"{test_exercise.format_exercise()}")
+    test_exercise.print_exercise()
---
+++ src/exercise.py
@@ -0,0 +1,34 @@
+class Exercise:
+    def __init__(self, name):
+        self.name = name
+        self.sets = 0
+        self.reps = 0
+        self.weight = 0
+        self.prev_weight = 0
+        self.rest_timer = 30
+
+        # self.sets = []
+        # GOAL: allow modular set changes
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_sets(self, sets):
+        self.sets = sets
+
+    def set_reps(self, reps):
+        self.reps = reps
+
+    def set_weight(self, weight):
+        self.prev_weight = self.weight
+        self.weight = weight
+
+    def set_rest_timer(self, rest_timer):
+        self.rest_timer = rest_timer
+
+    def print_exercise(self):
+        print(f"{self.name}: {self.sets}x{self.reps} ({self.weight}kg)")
+
+    def format_exercise(self):
+        # for writing to file
+        return f"{self.name}:{self.sets}:{self.reps}:{self.weight}:{self.prev_weight}"
