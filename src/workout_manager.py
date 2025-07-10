3b162d80666af5ab15adbbcf3655a55d33134e68
Jonas Inocencio <s3787701@student.rmit.edu.au>
2025-07-10 19:47:31 +0100

    created workout manager class to handle managing individual exercises within a workout

--- src/workout_manager.py
+++ src/workout_manager.py
@@ -1,3 +1,68 @@
+from src.exercise import Exercise
+
 class WorkoutManager:
     def __init__(self):
         self.exercises = []
+
+    def add_exercise(self):
+        # This should all be handled by the menu class
+        print("Name: ")
+        name = input()
+        exercise = Exercise(name)
+        print("Sets: ")
+        exercise.set_sets(input())
+        # ADD ERROR HANDLING
+        print("Reps: ")
+        exercise.set_reps(input())
+        # ADD ERROR HANDLING
+        print("Weight: ")
+        exercise.set_weight(input())
+        # ADD ERROR HANDLING
+
+        self.exercises.append(exercise)
+
+    def remove_exercise(self):
+        if not self.exercises:
+            print("No exercises.")
+            return
+        self.list_exercises()
+        print("REMOVE EXERCISE ('-1' to cancel): ")
+        choice = int(input())
+        # FIX: if 'choice' is not integer
+        if choice != '-1':
+            try:
+                self.exercises.pop(choice)
+            except IndexError:
+                print("Invalid index.")
+
+    # def overwrite_exercise(self, index, exercise):
+    #     self.exercises[index] = exercise
+
+    def clear_exercises(self):
+        self.exercises.clear()
+
+    def list_exercises(self):
+        if not self.exercises:
+            print("No exercises.")
+            return
+        for i, exercise in enumerate(self.exercises):
+            print(f"{i}: {exercise.name}")
+
+    # modify exercise??
+    #
+
+    def import_workout(self, data):
+        self.clear_exercises()
+        for ex in data:
+            temp = Exercise(ex["name"])
+            temp.set_sets(ex["sets"])
+            temp.set_reps(ex["reps"])
+            temp.set_weight(ex["weight"])
+            self.exercises.append(temp)
+        self.list_exercises()
+
+    def export_workout(self):
+        export = []
+        for exercise in self.exercises:
+            export.append(exercise.format_exercise())
+        return export
