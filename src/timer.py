7024c547a29272cb0c5e529320529e65adaf4635
Jonas Inocencio <s3787701@student.rmit.edu.au>
2025-07-11 21:32:36 +0100

    Added comments

--- src/timer.py
+++ src/timer.py
@@ -7,22 +7,24 @@ def format_time(seconds):
     return f"{minute:02d}:{second:02d}:{millisecond:03d}"
 
 class Timer:
-    # LOOKS AT:
-    # Time class (performance counter), updating outputs, etc.
-    # Threads
+    # FOCUS: time class, dynamic output, threads
     def __init__(self):
+        # Constructor
         self.start = 0
         self.end = 0
 
     def tick(self):
+        # 'Ticks' the clock forward .01 seconds and prints the formatted time
+        # Carriage makes the output appear as though it is updating, flush ensures accuracy
+        time.sleep(.01)
         elapsed = time.perf_counter() - self.start
         print(format_time(elapsed), end='\r', flush=True)
 
     def run(self):
+        # Runs a constantly updating stopwatch until the exit command (ctrl + c) is pressed
         self.start = time.perf_counter()
         try:
             while True:
-                time.sleep(.01)
                 self.tick()
         except KeyboardInterrupt:
             self.end = time.perf_counter()
@@ -31,6 +33,7 @@ class Timer:
             exit(0)
 
     def countdown(self, duration):
+        # Counts down until the provided duration
         self.start = time.perf_counter()
         while time.perf_counter() - self.start < duration:
             time.sleep(.01)
